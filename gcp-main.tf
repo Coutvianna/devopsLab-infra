059e9378aa5db30601405cc7c3de3fd1541f213c
Robson viana coutinho <robbson_viana@hotmail.com>
2023-03-23 22:58:26 -0300

    Artifact Registry

--- gcp-main.tf
+++ gcp-main.tf
@@ -20,3 +20,9 @@ resource "google_compute_instance" "firstvm" {
     }
   }
 }
+resource "google_artifact_registry_repository" "my-repo" {
+  location = var.region
+  repository_id = "labdevops"
+  description = "Imagens Docker"
+  format = "DOCKER"
+}
